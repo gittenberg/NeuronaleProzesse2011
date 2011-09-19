45eb7d6b912cfe0bb8276b78172aa47bcc906207
Martin <mjseeger@gmail.com>
2011-09-19 10:20:30 +0200

    Initial commit

---
+++ src/test.py
@@ -0,0 +1,6 @@
+from pyNN.neuron import * #doctest: +SKIP
+from pyNN.nest import *  #doctest: +SKIP
+from pyNN.pcsim import *  #doctest: +SKIP
+from pyNN.brian import *  #doctest: +SKIP
+
+print "hallo welt"
