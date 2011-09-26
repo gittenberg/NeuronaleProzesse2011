dae375b6fda980dea3d533fce45ffb5186b5d53d
martin <mjseeger@gmail.com>
2011-09-26 16:04:44 +0200

    ...

--- src/day5exercise2.py
+++ src/day5exercise2.py
@@ -2,7 +2,91 @@ from pylab import *
 import pyNN.neuron as p
 import time
 
-p.setup(timestep=0.1, quit_on_end=False)
+timestep = 0.1
+p.setup(timestep, quit_on_end=False)
+
+def exercise1(n_inhib=25, n_excit=100):
+    sspe = p.Population(n_excit, cellclass=p.SpikeSourcePoisson)
+    sspi = p.Population(n_inhib, cellclass=p.SpikeSourcePoisson)
+    sspe.set('rate', 10.)
+    sspi.set('rate', 10.)
+    sspe.record()
+    sspi.record()
+
+    n = p.Population(1, cellclass=p.IF_cond_exp)
+    n.record()
+    n.record_v()
+    n.record_gsyn()
+
+    prje = p.Projection(sspe, n, target="excitatory", method=p.AllToAllConnector())
+    prje.setWeights(0.0001)
+    prji = p.Projection(sspi, n, target="inhibitory", method=p.AllToAllConnector())
+    prji.setWeights(0.0001)
+
+    p.run(1000.)
+    pot = n.get_v()
+    spikes_e = sspe.getSpikes()
+    spikes_i = sspi.getSpikes()
+
+    subplot(2,1,1)
+    ylabel('Membrane potential/mV')
+    plot(pot[:,1], pot[:,2])
+
+    subplot(2,1,2)
+
+    for spike in spikes_i:
+        plt.plot(spike[1], spike[0], '.', markersize=1, color='g')
+    for spike in spikes_e:
+        plt.plot(spike[1], spike[0]+n_inhib, '.', markersize=1, color='r')
+
+    xlabel('Time/ms')
+    ylabel('Source ID')
+
+    ylim(0, n_inhib + n_excit)
+
+    savefig('../output/day5figure3.png')
+
+    figure()
+
+    window = 100
+    STAgroup = []
+    for spike in spikes_e:
+        startTime = int(spike[1])
+        if startTime > window*timestep and len(pot) > startTime + 2*window*timestep:
+            STAgroup.append(pot[startTime/timestep-window:startTime/timestep+2*window])
+    
+    timePoints = np.arange(0,3*window)
+
+
+    for STA in STAgroup:
+        if len(timePoints) == len(STA):
+            plt.plot(timePoints, STA[:,2], color='0.7')
+
+    valueGroup = []
+    for STA in STAgroup:
+        valueGroup.append(STA[:,2])
+
+
+    valueGroup = np.array(valueGroup)
+    averageSTA = []
+    for x in range(0, window):
+        averageSTA.append(np.average(np.array(valueGroup[:,0])))
+
+    
+    plot(timePoints, averageSTA)
+
+    figure()
+    xlabel('Time/ms')
+    ylabel(u'Inhibitory conductance/\u00B5S')
+
+    gsyn = n.get_gsyn()
+    plot(gsyn[:,1], gsyn[:,2], color='r')
+    plot(gsyn[:,1], gsyn[:,3], color='g')
+
+    savefig('../output/day5figure4.png')
+
+    draw()
+    show()
 
 '''
 def exercise1(v_rest = 65.0):
