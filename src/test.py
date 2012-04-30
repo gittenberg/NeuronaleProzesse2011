1e50ac7c29dad73371159ceabc81a2126e532ad6
Martin <mjseeger@gmail.com>
2012-04-30 23:21:05 +0200

    Minor

--- src/test.py
+++ src/test.py
@@ -1,9 +1,19 @@
 # test from laptop
 # test from Windows
 
-from pyNN.neuron import * #doctest: +SKIP
+
+#neuron.load_mechanisms('/usr/local/lib/python2.7/dist-packages/pyNN/neuron/nmodl')
+
+import pyNN
+import inspect
+
+print(pyNN.__file__)
+
+import pyNN.neuron as p
+
+#from pyNN.neuron import * #doctest: +SKIP
 from pyNN.nest import *  #doctest: +SKIP
-from pyNN.pcsim import *  #doctest: +SKIP
-from pyNN.brian import *  #doctest: +SKIP
+#from pyNN.pcsim import *  #doctest: +SKIP
+#from pyNN.brian import *  #doctest: +SKIP
 
 print "hallo welt"
